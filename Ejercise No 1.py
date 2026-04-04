51d3a3b52570bd882cff5eaf1cb9e93ad2506015
Rey-Orellana <reyorellana13@gmail.com>
2026-04-04 16:20:01 -0400

    Aprendiendo a ausar f para imprimir dentro de un print variables en python

--- Ejercise No 1.py
+++ Ejercise No 1.py
@@ -1,3 +1,14 @@
 ##Comentarios en Python
+#en Python se imprimero con la fguncion Print
+print("Me gusta la pizza..!!!")
 
-print("Me gusta la pizza..!!!");
+
+##Variables en Python
+
+
+nombre = "Rey"
+
+apellido = "Orellana"
+
+
+print(f"Hola como estas {nombre} {apellido}")
