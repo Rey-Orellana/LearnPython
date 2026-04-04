141f803f5c58da6d42a7d21413e792c756829b5e
Rey-Orellana <reyorellana13@gmail.com>
2026-04-04 16:24:06 -0400

    Aprendiendoa usar f antes de textos en Python

--- Ejercise No 1.py
+++ Ejercise No 1.py
@@ -9,6 +9,16 @@ print("Me gusta la pizza..!!!")
 nombre = "Rey"
 
 apellido = "Orellana"
+estatura = 1,80
 
+sexo = 'M'
 
-print(f"Hola como estas {nombre} {apellido}")
+estudiante = False
+
+valor = ""
+if(estudiante):
+    valor = " estudiante"
+else:
+    valor = " pandillero"
+
+print(f"Hola como estas {nombre} {apellido} eres {valor} eres de sexo {sexo} y mides {estatura}")
