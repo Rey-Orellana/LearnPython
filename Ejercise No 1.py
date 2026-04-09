9a8ffad721a78489f48cd1b092a4e580f630e372
Rey-Orellana <reyorellana13@gmail.com>
2026-04-08 23:18:06 -0400

    Diccionario en Python

--- Ejercise No 1.py
+++ Ejercise No 1.py
@@ -1,28 +1,8 @@
-#recorriendo una list
-
-l = [1,2,3,4,5,6]
-
-print(l[0:6:3])
-
-
-#Colecciones en python
-
-
-l = [1,2,3,4,5,6]
-
-print(l[0:6:3])
-
-tuplas = (2,4,6,7,8)
-
-print(tuplas[0:5:2])
+#Diccionario en Python
 
 diccionario = {
-    "nombre":"Iron Man",
-    "apellido":"Stark",
-    "edad":24,
-    "mayor":True
+    "nombre": "Juan",
+    "apellido":"Macario",
+    "edad":17,
+    "mayor_de_edad": False
 }
-
-
-print(diccionario)
-print(diccionario["apellido"])
