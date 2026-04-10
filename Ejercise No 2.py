7c866b5c1b752b95b704d4113a779f6c30d30347
Rey-Orellana <reyorellana13@gmail.com>
2026-04-09 22:13:53 -0400

    Haciendo uso de elif

--- Ejercise No 2.py
+++ Ejercise No 2.py
@@ -1,8 +1,6 @@
-a = 200
-b = 33
-if b > a:
-  print("b is greater than a")
-elif a == b:
-  print("a and b are equal")
+number = 7
+
+if number % 2 == 0:
+  print("The number is even")
 else:
-  print("a is greater than b")
+  print("The number is odd")
