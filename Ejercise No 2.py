df57a5a5b1241ede364f1fe69011c8206a636117
Rey-Orellana <reyorellana13@gmail.com>
2026-04-09 22:04:43 -0400

    Usando Elif

--- Ejercise No 2.py
+++ Ejercise No 2.py
@@ -1,16 +1,8 @@
-day = 3
-
-if day == 1:
-  print("Monday")
-elif day == 2:
-  print("Tuesday")
-elif day == 3:
-  print("Wednesday")
-elif day == 4:
-  print("Thursday")
-elif day == 5:
-  print("Friday")
-elif day == 6:
-  print("Saturday")
-elif day == 7:
-  print("Sunday")
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
