f708bc7a35d18d9c52918d39cfb468107d7de38f
Rey-Orellana <reyorellana13@gmail.com>
2026-04-12 23:25:15 -0400

    Aprendiendo a realizar ejercicios en Python

--- Ejercise No 2.py
+++ Ejercise No 2.py
@@ -1,18 +1,4 @@
-# TYPE: Devuelve el tipo de dato de una variable
+l = ["Platano", "Manzana", "Uva", "Kiwi"]
 
-# Tipos comunes
-nombre = "Juan"
-edad = 30
-altura = 1.75
-es_mayor = True
-colores = ["rojo", "verde"]
-
-print(type(nombre))   # <class 'str'>
-print(type(edad))     # <class 'int'>
-print(type(altura))   # <class 'float'>
-print(type(es_mayor)) # <class 'bool'>
-print(type(colores))  # <class 'list'>
-
-# Útil para depuración
-valor = input("Ingresa algo: ")
-print(f"El tipo de lo que ingresaste es: {type(valor)}")
+for fruta in l:
+    print(fruta)
