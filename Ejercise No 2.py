3bac8b4d3a6d7ed01935646e689885e971868b9b
Rey-Orellana <reyorellana13@gmail.com>
2026-04-15 23:31:25 -0400

    Robot

--- Ejercise No 2.py
+++ Ejercise No 2.py
@@ -1,5 +1,7 @@
-class Mascota:
-    pass
+class Robot:
+    def __init__(self, nombre, modelo):
+        self.nombre = nombre
+        self.modelo = modelo
 
-mi_perro = Mascota()
-print(type(mi_perro))
+mi_robot = Robot("R2D2", "Astromecánico")
+print(f"Hola, soy {mi_robot.nombre}")
