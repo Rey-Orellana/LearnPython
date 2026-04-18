6b3d339efac491b218c06afefce8f6869f7a58e4
Rey-Orellana <reyorellana13@gmail.com>
2026-04-17 22:56:16 -0400

    Aprendiendo a Arrays

--- Ejercise No 2.py
+++ Ejercise No 2.py
@@ -1,6 +1,8 @@
-mytuple = ("apple", "banana", "cherry")
-myit = iter(mytuple)
+arraysitos = []
+arraysitos[0]=0
+arraysitos[1]=1
+arraysitos[2]=2
+arraysitos[3]=3
+arraysitos[4]=3
 
-print(next(myit))
-print(next(myit))
-print(next(myit))
+print(arraysitos)
