7001e94f5f7b5a98435e8010d8ee85eaaed4e845
Rey-Orellana <reyorellana13@gmail.com>
2026-04-18 23:48:33 -0400

    Agregando funciones

--- Saludo.py
+++ Saludo.py
@@ -1,21 +1,7 @@
 #Listas en Python
 
-lista = {1,2,3}
-
-lista.add(4)
-
-print(lista)
+def miNombre(nombre):
+    print(f"Mi nombre es: {nombre}")
 
 
-tupla = ("Dos","seis",5,8)
-
-print(type(tupla))
-
-diccionarios = {"nombre":"Rey","Apellido":"Orellana","edad":27,"Sexo":"Masculibi","Estudiante":False}
-
-print(diccionarios)
-print(type(diccionarios))
-
-#Aprendiendo algunas cosas
-
-print(diccionarios["nombre"])
+miNombre("Rey")
