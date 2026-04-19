8ff1de897ab207c8c46a34db475c6d9a7b52586c
Rey-Orellana <reyorellana13@gmail.com>
2026-04-18 23:45:46 -0400

    Agregando diccionarios

--- Saludo.py
+++ Saludo.py
@@ -10,3 +10,8 @@ print(lista)
 tupla = ("Dos","seis",5,8)
 
 print(type(tupla))
+
+diccionarios = {"nombre":"Rey","Apellido":"Orellana","edad":27,"Sexo":"Masculibi","Estudiante":False}
+
+print(diccionarios)
+print(type(diccionarios))
