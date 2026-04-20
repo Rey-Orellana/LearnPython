ad670ad83826a77c385235f32b95633765adc782
Rey-Orellana <reyorellana13@gmail.com>
2026-04-19 23:02:56 -0400

    Aprendiendo Saludo

--- Saludo.py
+++ Saludo.py
@@ -1,14 +1,13 @@
-#Listas en Python
+#Clases en Python
 
-def miNombre(nombre):
-    mayusculas =nombre.upper()
-    return mayusculas
+class Vehiculo:
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+    
+    def describir(self):
+        return f"Este es un {self.marca} modelo {self.modelo}"
+    
 
-
-print(miNombre("Rey"))
-
-vairablePequena = "ORALE"
-
-minuscula = vairablePequena.lower()
-
-print(minuscula)
+mi_auto = Vehiculo("Toyota","Corolla")
+print(mi_auto.describir())
