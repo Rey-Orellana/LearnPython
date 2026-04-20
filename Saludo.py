b4fc6ecd963af90c16588811ed151b9cca1ec622
Rey-Orellana <reyorellana13@gmail.com>
2026-04-19 23:03:58 -0400

    Aprendiendo cases herencias

--- Saludo.py
+++ Saludo.py
@@ -1,13 +1,11 @@
-#Clases en Python
+class CocheElectrico(Vehiculo): # Hereda de Vehiculo
+    def __init__(self, marca, modelo, bateria):
+        super().__init__(marca, modelo) # Llama al constructor del padre
+        self.bateria = bateria
 
-class Vehiculo:
-    def __init__(self, marca, modelo):
-        self.marca = marca
-        self.modelo = modelo
-    
-    def describir(self):
-        return f"Este es un {self.marca} modelo {self.modelo}"
-    
+    def mostrar_bateria(self):
+        return f"Batería al {self.bateria}%."
 
-mi_auto = Vehiculo("Toyota","Corolla")
-print(mi_auto.describir())
+teslita = CocheElectrico("Tesla", "Model 3", 90)
+print(teslita.describir())
+print(teslita.mostrar_bateria())
