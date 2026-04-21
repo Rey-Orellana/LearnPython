65d77cfecb5bcaf7297678b3dc7704458762e7b8
Rey-Orellana <reyorellana13@gmail.com>
2026-04-20 23:46:01 -0400

    Aprendiendo a usar Python

--- Saludo.py
+++ Saludo.py
@@ -1,20 +1,6 @@
-class Motor:
-    def __init__(self, tipo):
-        self.tipo = tipo
+class Perro:
+    pass  # Una clase vacía por ahora
 
-    def encender(self):
-        return f"Motor {self.tipo} rugiendo..."
-
-class Auto:
-    def __init__(self, marca, motor):
-        self.marca = marca
-        self.motor = motor  # El auto "tiene un" motor (Composición)
-
-    def arrancar(self):
-        return f"{self.marca}: {self.motor.encender()}"
-
-# Creamos las piezas por separado
-mi_motor = Motor("V8")
-mi_nave = Auto("Ford", mi_motor)
-
-print(mi_nave.arrancar())
+mi_perro = Perro()
+print(type(mi_perro)) 
+# Salida: <class '__main__.Perro'>
