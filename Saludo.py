aabae0edcf9957b2ac674ec2f4b39812ca60f5a7
Rey-Orellana <reyorellana13@gmail.com>
2026-04-20 23:46:28 -0400

    Gato

--- Saludo.py
+++ Saludo.py
@@ -1,6 +1,7 @@
-class Perro:
-    pass  # Una clase vacía por ahora
+class Gato:
+    def __init__(self, nombre, color):
+        self.nombre = nombre  # Atributo
+        self.color = color    # Atributo
 
-mi_perro = Perro()
-print(type(mi_perro)) 
-# Salida: <class '__main__.Perro'>
+michi = Gato("Felix", "Naranja")
+print(f"Mi gato se llama {michi.nombre} y es de color {michi.color}")
