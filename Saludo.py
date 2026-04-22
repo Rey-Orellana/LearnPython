ff09722bcbfc0f2773c1d490d1ac3e9cadfabdc0
Rey-Orellana <reyorellana13@gmail.com>
2026-04-21 23:45:09 -0400

    Haciendo Person clase

--- Saludo.py
+++ Saludo.py
@@ -1,3 +1,8 @@
-x = "Hello World!"
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age  # Private property
 
-print(len(x))
+p1 = Person("Emil", 25)
+print(p1.name)
+print(p1.__age)  # This will cause an error
