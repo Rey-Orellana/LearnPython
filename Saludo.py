b9981c4495f2deff33dbe8c5e6931ccb3f54af42
Rey-Orellana <reyorellana13@gmail.com>
2026-04-21 23:48:42 -0400

    Aprendiendo a usar otras librerias

--- Saludo.py
+++ Saludo.py
@@ -1,5 +1,16 @@
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
 import numpy
+import matplotlib.pyplot as plt
 
-x = numpy.random.uniform(0.0, 5.0, 250)
+x = numpy.random.uniform(0.0, 5.0, 100000)
 
-print(x)
+plt.hist(x, 100)
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
