8eb24b3168ed73b071ba1c77fa094e1d67ad4726
Rey-Orellana <reyorellana13@gmail.com>
2026-04-21 23:47:17 -0400

    Aprendiendo a usar clases ene python

--- Saludo.py
+++ Saludo.py
@@ -1,20 +1,9 @@
-class Calculator:
-  def __init__(self):
-    self.result = 0
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.__age = age
 
-  def __validate(self, num):
-    if not isinstance(num, (int, float)):
-      return False
-    return True
+p1 = Person("Emil", 30)
 
-  def add(self, num):
-    if self.__validate(num):
-      self.result += num
-    else:
-      print("Invalid number")
-
-calc = Calculator()
-calc.add(10)
-calc.add(5)
-print(calc.result)
-# calc.__validate(5)  # This would cause an error
+# This is how Python mangles the name:
+print(p1._Person__age)  # Not recommended!
