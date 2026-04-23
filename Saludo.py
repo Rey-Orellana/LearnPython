41d1dd46e58d962cc9b9d8a78cb6dc5790c19e6b
Rey-Orellana <reyorellana13@gmail.com>
2026-04-22 23:12:19 -0400

    Aprendiendo a realizar cosas faciles

--- Saludo.py
+++ Saludo.py
@@ -1,3 +1,4 @@
-colores = ["rojo", "azul", "verde"]
-colores.remove("azul")
-print(colores)  # ['rojo', 'verde']
+letras = ['a', 'b', 'c', 'd']
+eliminada = letras.pop(2)  # elimina índice 2 ('c')
+print(letras)      # ['a', 'b', 'd']
+print(eliminada)   # c
