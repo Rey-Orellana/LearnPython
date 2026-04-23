223753fda9173921306fd692302fbf0c01beff59
Rey-Orellana <reyorellana13@gmail.com>
2026-04-22 23:11:54 -0400

    Facilmente aprendiendo a usar Python mas javita

--- Saludo.py
+++ Saludo.py
@@ -1,3 +1,3 @@
-numeros = [1, 2, 4]
-numeros.insert(2, 3)  # posición 2, valor 3
-print(numeros)  # [1, 2, 3, 4]
+colores = ["rojo", "azul", "verde"]
+colores.remove("azul")
+print(colores)  # ['rojo', 'verde']
