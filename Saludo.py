6374867853263e05f54f6f99393062b28bc20ec1
Rey-Orellana <reyorellana13@gmail.com>
2026-04-23 23:38:29 -0400

    Usando un poco de Ciclos con matrices

--- Saludo.py
+++ Saludo.py
@@ -1,3 +1,4 @@
-notas = {"Derecho": 90, "Sistemas": 95, "Inglés": 60}
-aprobados = {k: v for k, v in notas.items() if v >= 70}
-print(f"Materias aprobadas: {aprobados}")
+matriz = [[1, 2], [3, 4]]
+for fila in matriz:
+    for num in fila:
+        print(f"Número: {num}")
