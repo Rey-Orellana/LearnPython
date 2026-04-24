663264b32b2fc7157e89315b6a8168e6ec3dcf69
Rey-Orellana <reyorellana13@gmail.com>
2026-04-23 23:37:00 -0400

    Presurisado

--- Saludo.py
+++ Saludo.py
@@ -1,3 +1,3 @@
-#Hola papus y mamus como estan wecolores = ("rojo", "verde", "azul")
-for color in colores:
-    print(f"Color: {color}")
+usuario = {"nombre": "Alex", "rol": "Admin", "puntos": 150}
+for llave, valor in usuario.items():
+    print(f"{llave.capitalize()}: {valor}")
