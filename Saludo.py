5fefa1e1b4bb58f466dc553ef8d61a9b319871a7
Rey-Orellana <reyorellana13@gmail.com>
2026-04-24 22:59:35 -0400

    Operacion nuemero 3

--- Saludo.py
+++ Saludo.py
@@ -1,4 +1,5 @@
-# 2. Vector con NumPy (recomendado)
-import numpy as np
-vector2 = np.array([1, 2, 3, 4])
-print("Vector NumPy:", vector2)
+# 3. Suma de vectores
+v1 = np.array([1, 2, 3])
+v2 = np.array([4, 5, 6])
+suma = v1 + v2
+print("Suma:", suma)  # [5 7 9]
