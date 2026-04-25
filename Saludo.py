db6aed28fc61c7fd270ef083be92ab8cb7527082
Rey-Orellana <reyorellana13@gmail.com>
2026-04-24 22:59:53 -0400

    Cuatro

--- Saludo.py
+++ Saludo.py
@@ -1,5 +1,4 @@
-# 3. Suma de vectores
-v1 = np.array([1, 2, 3])
-v2 = np.array([4, 5, 6])
-suma = v1 + v2
-print("Suma:", suma)  # [5 7 9]
+# 4. Multiplicación por escalar
+escalar = 2
+producto = escalar * v1
+print("Escalar * vector:", producto)  # [2 4 6]
