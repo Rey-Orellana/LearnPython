4189e21f4705c3efb5c46e4bd6c6529dc61af9d7
Rey-Orellana <reyorellana13@gmail.com>
2026-04-25 23:57:01 -0400

    Aprobado

--- Saludo.py
+++ Saludo.py
@@ -1,3 +1,3 @@
-tareas = ["Lavar ropa", "Estudiar Python", "Ir al gym"]
-for i, tarea in enumerate(tareas, start=1):
-    print(f"{i}. {tarea}")
+productos = [("Laptop", 800), ("Mouse", 25), ("Teclado", 50)]
+for nombre, precio in productos:
+    print(f"Producto: {nombre} | Precio: ${precio}")
