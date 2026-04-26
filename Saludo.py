1adcca045df55ea5fdc8147c78f436187923d085
Rey-Orellana <reyorellana13@gmail.com>
2026-04-25 23:56:25 -0400

    Paloza

--- Saludo.py
+++ Saludo.py
@@ -1,3 +1,6 @@
-capitales = {"Bolivia": "Sucre", "México": "CDMX", "España": "Madrid"}
-for pais, capital in capitales.items():
-    print(f"La capital de {pais} es {capital}")
+edades = [12, 25, 18, 40, 15, 30]
+mayores = []
+for edad in edades:
+    if edad >= 18:
+        mayores.append(edad)
+print(f"Usuarios mayores de edad: {mayores}")
