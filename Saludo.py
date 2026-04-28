8456284686ea25b45ddabd82669062e5e2b560f5
Rey-Orellana <reyorellana13@gmail.com>
2026-04-27 23:41:41 -0400

    Python manzana

--- Saludo.py
+++ Saludo.py
@@ -1,6 +1,6 @@
 import json
 
-with open("datos.json", "r") as file:
-    data = json.load(file)
+data = {"id": 1, "items": ["manzana", "pera"], "activo": True}
+pretty_json = json.dumps(data, indent=4)
 
-print(data)
+print(pretty_json)
