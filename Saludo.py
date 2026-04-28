5650c4bf6af6164cda2088f25150854e515999ed
Rey-Orellana <reyorellana13@gmail.com>
2026-04-27 23:41:18 -0400

    Usando Json

--- Saludo.py
+++ Saludo.py
@@ -1,6 +1,6 @@
 import json
 
-data = {"usuario": "admin", "puntos": 1500}
+with open("datos.json", "r") as file:
+    data = json.load(file)
 
-with open("datos.json", "w") as file:
-    json.dump(data, file)
+print(data)
